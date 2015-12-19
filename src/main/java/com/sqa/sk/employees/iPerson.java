44d12c05de5364afe4c23908e31b48314604d8ba
sevakogan <sevakogan@sevas-mbp.gateway.pace.com>
2015-12-19 15:57:20 -0800

    creating iPerson Intrface

---
+++ src/main/java/com/sqa/sk/employees/iPerson.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: iPerson.java<br>
+ * LastName, FirstName<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Dec 19, 2015
+ */
+package com.sqa.sk.employees;
+
+/**
+ * iPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ */
+public interface IPerson {
+
+	final static int MAX_AGE = 110;
+
+	public String getName();
+
+	public boolean move();
+}
